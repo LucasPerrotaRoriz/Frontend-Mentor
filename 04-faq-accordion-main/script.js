40561b07e2be64ae27e87abe96876be036022194
lucassensei <Lucaswebdeveloper@proton.me>
2024-05-22 15:12:14 -0300

    Project almost done

--- 04-faq-accordion-main/script.js
+++ 04-faq-accordion-main/script.js
@@ -1,42 +1,19 @@
-const tabs = document.querySelectorAll('.tab__item');
-const icons = document.querySelectorAll('.tab__icon');
-const answers = document.querySelectorAll('.tab__answer');
+const buttons = document.querySelectorAll('.accordion__btn')
 
-const src = ['./assets/images/icon-plus.svg', './assets/images/icon-minus.svg'];
-
-
-/* 
-
-
-function reset() {
-  tabs.forEach((tab, idx) => {
-    tab.style.maxHeight = getComputedStyle(answers[idx]).height;
-    tabs[idx].dataset.opened = 'false';
+buttons.forEach(btn => {
+  btn.addEventListener('click', function(e) {
+    const accordionDesc = this.nextElementSibling
+    const plusIcon = this.querySelector('.plus__icon');
+    const minusIcon = this.querySelector('.minus__icon');
     
-    icons.forEach((icon) => {
-      icon.classList.contains('tab__icon-plus') 
-        ? icon.style.display = 'block' 
-        : icon.style.display = 'none'
-    })
-  });
-}
-reset();
-
-icons.forEach((icon, idx) => {
-  icon.addEventListener('click', function (e) {
-    reset();
-    const currentIcon = e.target;
-    const tabItem = currentIcon.closest('.tab__item');
-    if (!tabItem.dataset.opened) {
-      currentIcon.style.display = 'none';
-      currentIcon.nextElementSibling.style.display = 'block'
-      tabItem.style.overflow = 'auto';
-      tabItem.style.maxHeight = '156px';
-      tabItem.dataset.opened = true;
+    if(accordionDesc.style.maxHeight) {
+      accordionDesc.style.maxHeight = null;
+      plusIcon.style.display = 'block';
+      minusIcon.style.display = 'none';
+    } else {
+      accordionDesc.style.maxHeight = accordionDesc.scrollHeight + 'px';
+      plusIcon.style.display = 'none';
+      minusIcon.style.display = 'block';
     }
-  });
-});
-
-
-
-*/
+  })  
+})
